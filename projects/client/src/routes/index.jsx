42646f31eb2def6bc9322c63999c778e81b4ba89
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2023-08-31 15:49:44 +0700

    Homepage v.1
    Homepage with dummy data

--- projects/client/src/routes/index.jsx
+++ projects/client/src/routes/index.jsx
@@ -1,7 +1,18 @@
 import { Route, createRoutesFromElements } from "react-router-dom";
 
-import Homepage from "../pages/Home";
+import { Homepage } from "../pages/Home";
+import { Register } from "../pages/Register";
+import { Login } from "../pages/Login";
+import { HomepageView } from "../views/Home";
 
-const Routes = <Route path="/" element={<Homepage />} />;
+const Routes = (
+    <>
+        <Route path="/" element={<Homepage />}>
+            <Route path="" element={<HomepageView />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+    </>
+);
 
 export const routes = createRoutesFromElements(Routes);
