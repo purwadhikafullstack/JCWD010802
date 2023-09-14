e3ebdc4b567fcf6844585c880018b2a58adf853d
faridsuryadi <132431667+faridsuryadi@users.noreply.github.com>
2023-09-14 13:31:05 +0700

    [MW-22] As a user, I want to update product data in my cart (#24)

--- projects/client/src/views/Cart/components/cartCheckout.jsx
+++ projects/client/src/views/Cart/components/cartCheckout.jsx
@@ -1,7 +1,9 @@
 import { Box, Button, Divider, HStack, Heading, Text, VStack } from "@chakra-ui/react"
 import formatIDR from "../../../helpers/formatIDR"
 
-export const CartCheckout = (cart) =>{
+export const CartCheckout = ({cart,total}) =>{
+    let totalPrice = 0
+    console.log(cart);
     return(
         <>
           <Box
@@ -9,22 +11,39 @@ export const CartCheckout = (cart) =>{
           borderWidth="1px"
           borderRadius="md"
           boxShadow={"md"}
-          w={"20%"}
+
+          w={"25%"}
+          ml={10}
+          mt={10}
           h={"fit-content"}
         >
             <Box>
-                <Heading fontSize={"2xl"}>
+                <Heading fontSize={"2xl"} mb={3}>
                     Cart Summary
                 </Heading>
-                <Text color={"gray.400"}> Total Item ({cart.length})</Text>
+                {cart.map((item)=>{
+                     const subtotal = item.product.price * item.quantity;
+                     totalPrice += subtotal;
+                     return (
+                    <HStack justifyContent={"space-between"}>
+                       <Text color={"gray.500"} fontSize={"18px"}>
+                {item.product.name} ({item.quantity}{" "}
+                {item.quantity > 1 ? "items" : "item"})
+              </Text>
+                        <Text color={"gray.500"} fontSize={"18px"}>{formatIDR (subtotal)}
+                        </Text>
+
+                    </HStack>
+                     )
+                })}
             </Box>
-            <Divider my={5}/>
+            <Divider my={5} borderWidth={4}/>
             <HStack justifyContent={"space-between"}>
             <Heading fontSize={"2xl"}>
                     Total Price
             </Heading>
             <Heading fontSize={"2xl"}>
-                {formatIDR(cart.price)}
+                {formatIDR(total)}
             </Heading>
             </HStack>
             <Button colorScheme="green" mt={5} w={"full"}>
--- projects/client/src/views/Cart/components/cartItem.jsx
+++ projects/client/src/views/Cart/components/cartItem.jsx
@@ -1,36 +1,79 @@
-import React from 'react';
-import { Box, Text, Image, Button, Flex, HStack } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Box, Text, Image, Button, Flex, HStack, Heading, Divider } from '@chakra-ui/react';
 import { Counter } from '../../Product/components/counter';
 import { CartCounter } from './counter';
 import { DeleteCart } from './deleteCart';
 import formatIDR from '../../../helpers/formatIDR';
+import { BsFillTrash3Fill } from 'react-icons/bs';
+import axios from 'axios';
 
-export const CartItem = ({ cart }) => {
+export const CartItem = ({ cart,reload,setReload }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [itemToDelete, setItemToDelete] = useState(null);
+ 
+  const handleDelete = async (itemId) => {
+    try {
+      await axios.delete(`http://localhost:8000/api/cart/${itemId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+  
+      setIsModalOpen(false);
+      setItemToDelete(null)
+      setReload(!reload)
+  
+    
+    } catch (error) {
+      console.error(error);
+    }
+  };
+ 
   return (
     <Flex direction="column" w={"60%"}>
+        <Heading>Cart</Heading>
+        <Divider borderWidth={4} mt={3}/>
       {cart.map((item) => (
+        <>
         <Box
           key={item.id}
           p={4}
-          borderWidth="1px"
           borderRadius="md"
-          mb={4}
           display="flex"
           flexDirection="row"
         >
-          <Image src={`http://localhost:8000/productImg/${item.product.productImg}`} alt={item.product.name} maxH="100px" maxW="100px" />
+          <Image src={`http://localhost:8000/productImg/${item.product.productImg}`} alt={item.product.name} h="100px" w="100px" objectFit={"cover"}/>
           <Box ml={4} flex="1">
             <Text fontSize="xl" fontWeight="bold">
               {item.product.name}
             </Text>
             <Text mt={2}>Price: {formatIDR( item.product.price)}</Text>
-            <HStack justifyContent={"flex-end"} gap={8}>
-        <DeleteCart/>
-        <CartCounter initialCount={item.quantity}/>
-                  </HStack>
+            <HStack justifyContent={"flex-end"} gap={8} mt={10}>
+              <Text color={"gray.500"}>Move to Wishlist</Text>
+              <Divider orientation='vertical' h={6} borderWidth={2}/>
+              <Button
+            onClick={() => {
+            setIsModalOpen(true);
+            setItemToDelete(item.productId)
+            }}
+            bg={"transparent"}
+          >
+            <BsFillTrash3Fill size={"15px"}/>
+          </Button>       
+           <CartCounter initialCount={item.quantity} productId={item.productId}/>
+        </HStack>
           </Box>
         </Box>
+          <Divider borderWidth={4}/>
+      </>
       ))}
+  <DeleteCart
+  isOpen={isModalOpen}
+  onClose={() => {
+    setIsModalOpen(false);
+  }}
+  onDelete={() => handleDelete(itemToDelete)}
+/>
     </Flex>
   );
 };
