ff080ac9dc0788824ec7e931f994b966de74f8f5
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2023-10-09 11:18:21 +0700

    [MW-27-FIXED] As a user, I want to see my order list

--- projects/server/controllers/orderController.js
+++ projects/server/controllers/orderController.js
@@ -15,16 +15,13 @@ module.exports = {
             } = req.body; 
             const findCartItem = await cartItem.findAll({ where: { cartId: cartId } });
             const findAddress = await address.findOne({ where: { id: addressId } });
-            const findCart = await cart.findOne({where:{userId:req.user.id, isCheckOut:false}})
+            const findCart = await cart.findOne({ where: { userId: req.user.id, isCheckOut: false } });
             const warehouses = await warehouse.findAll({ include: { model: address } });
-            
-
-            let orderItemId
-
+    
+            let orderItemIds = [];     
             if (warehouses.length === 0) {
                 return res.status(404).send({ error: 'No warehouses found.' });
             }
-
             let nearestWarehouse = null;
             let shortestDistance = Infinity;
             warehouses.forEach((warehouse) => {
@@ -39,57 +36,58 @@ module.exports = {
                     nearestWarehouse = warehouse.id;
                 }
             });
-
+    
             if (!nearestWarehouse) {
                 return res.status(404).json({ error: 'Nearest warehouse not found.' });
             }
             const productQuantityToSubtract = new Map();
-
+    
             for (const item of findCartItem) {
                 const sproduct = await product.findByPk(item.productId);
-
+    
                 if (!sproduct) {
                     return res.status(404).send({ error: 'Product not found.' });
                 }
-
+    
                 const stockEntry = await stock.findOne({
                     where: {
                         productId: sproduct.id,
                         warehouseId: nearestWarehouse,
                     },
                 });
-
-
+    
                 const items = await orderItem.create({
                     productId: item.productId,
                     quantity: item.quantity
-                })
-
-                orderItemId = items.id
-
+                });
+    
+                orderItemIds.push(items.id);
+    
                 const currentQuantityToSubtract = productQuantityToSubtract.get(sproduct.id) || 0;
                 productQuantityToSubtract.set(sproduct.id, currentQuantityToSubtract + item.quantity);
             }
             const expireDate = new Date()
             expireDate.setDate(expireDate.getDate() + 1)
             const response = await orders.create({
-                userId:req.user.id,
-                totalPrice:findCart.totalPrice,
+                userId: req.user.id,
+                totalPrice: findCart.totalPrice,
                 shippingCost,
                 shippingMethod,
                 addressId,
                 cartId,
-                statusId:1,
+                statusId: 1,
                 warehouseId: nearestWarehouse,
                 paymentExpiredAt: expireDate
-            })
-
-            await orderItem.update({
-                orderId: response.id
-            },{
-                where: { id: orderItemId }
-            })
-
+            });
+    
+            
+            await orderItem.update(
+                { orderId: response.id },
+                {
+                    where: { id: orderItemIds }
+                }
+            );
+    
             for (const [productId, totalQuantity] of productQuantityToSubtract.entries()) {
                 const stockEntry = await stock.findOne({
                     where: {
@@ -97,23 +95,22 @@ module.exports = {
                         warehouseId: nearestWarehouse,
                     },
                 });
-
+    
                 if (stockEntry) {
                     stockEntry.quantity -= totalQuantity;
                     await stockEntry.save();
-
+    
                     const newJournal = await journal.create({
-                     description: "reduce",
-                     quantity: -totalQuantity,
-                     stockId: stockEntry.id,
-                     orderId: response.id
-                    })
+                        description: "reduce",
+                        quantity: -totalQuantity,
+                        stockId: stockEntry.id,
+                        orderId: response.id
+                    });
                 }
             }
-
+    
             const stockWarehouse = await stock.findAll({ where: { warehouseId: nearestWarehouse } });
-
-
+    
             const cartCheckout = await cart.update(
                 { isCheckOut: true },
                 {
@@ -122,14 +119,14 @@ module.exports = {
                     }
                 }
             );
-            
+    
             res.status(200).send({
                 response,
                 cartCheckout,
                 stockWarehouse,
                 status: true
             });
-
+    
         } catch (error) {
             console.error(error);
             res.status(500).send({ error: 'Internal server error.' });
