32bf1479c733f1c306342467a68bf1ecbb45b06b
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2023-10-18 12:27:24 +0700

    multer-test

--- projects/server/src/routers/bannerRouter.js
+++ projects/server/src/routers/bannerRouter.js
@@ -1,13 +1,12 @@
 const { bannerController } = require("../controllers")
 const { verifyToken } = require("../middlewares/auth")
 const { multerUpload } = require("../middlewares/multer")
-const path = require("path");
 
 const router = require("express").Router()
 
-router.post("/", verifyToken, multerUpload(path.join(__dirname,'../../public/bannerImg'), 'bannerImg').single('file'), bannerController.uploadBanner)
+router.patch("/", verifyToken, multerUpload('/bannerImg', 'bannerImg').single('file'), bannerController.uploadBanner)
 router.get("/", bannerController.getBanner)
-router.post("/", verifyToken, multerUpload('./public/bannerImg', 'bannerImg').single('file'), bannerController.addBanner)
+router.post("/", verifyToken, multerUpload('/bannerImg', 'bannerImg').single('file'), bannerController.addBanner)
 router.put("/:id", verifyToken, bannerController.deleteBanner)
 
 module.exports = router
