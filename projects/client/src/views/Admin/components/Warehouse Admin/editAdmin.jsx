794c60d5d7f83cd027967bf2ccd927e30696cdd9
Muhammad Farid Suryadi <132431667+faridsuryadi@users.noreply.github.com>
2023-10-21 01:49:12 +0700

    Mw 39 bug fix (#133)
    * fix mutation
    
    * Update adminOrderController.js
    
    * bug warehouse name

--- projects/client/src/views/Admin/components/Warehouse Admin/editAdmin.jsx
+++ projects/client/src/views/Admin/components/Warehouse Admin/editAdmin.jsx
@@ -52,7 +52,7 @@ export const AdminEditForm = ({ admin, onUpdateAdmin}) => {
     catch (error) {
       console.error(error); 
       setSubmitting(false);
-      toast.error(`${error?.response?.data?.error?.message}`)
+      toast.error(error?.response?.data?.message)
     }
   };
 
